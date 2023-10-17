78d1147b4b05930685c4c3b4b4e5c00bcf26a70a
RuturajMane73137 <ruturajmane1999@gmail.com>
2023-10-17 22:54:17 +0530

    Assignemt 7 added

---
+++ Assignment7/src/assignment7/WordCount.java
@@ -0,0 +1,26 @@
+package assignment7;
+import java.util.StringTokenizer;
+
+public class WordCount {
+
+	public static void main(String[] args) {
+		
+		int count=0;
+		String s1 = "Saurabh is a good boy";
+		System.out.println(s1);
+		
+		
+		StringTokenizer str = new StringTokenizer(s1,":/.",true);
+		
+		
+		while(str.hasMoreTokens()) {
+			String token = str.nextToken();
+			count++;
+			System.out.println(token);
+		}
+		
+		System.out.println("Total Strings = "+count);
+
+	}
+
+}
